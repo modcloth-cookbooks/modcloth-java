46434fab723468f59ed02d51e350e3d2eca1a654
Dan Buch <d.buch@modcloth.com>
2013-07-11 10:38:26 -0400

    Minor housekeeping

---
+++ README.md
@@ -0,0 +1,4 @@
+modcloth-java Cookbook
+======================
+
+Hey, it's ModCloth's take on installing java!
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name             "modcloth-java"
-maintainer       "ModCloth, Inc."
-maintainer_email "platformsphere@modcloth.com"
-license          "All Rights Reserved"
-description      "Installs java"
-version          "0.1.0"
+name             'modcloth-java'
+maintainer       'ModCloth, Inc.'
+maintainer_email 'platformsphere@modcloth.com'
+license          'All Rights Reserved'
+description      'Installs java'
+version          '0.1.0'
