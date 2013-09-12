72635c703ae9d5659fd86ac3bf5cd6d250f857ab
Rafe Colton <r.colton@modcloth.com>
2013-09-12 17:42:22 -0400

    Install jdk if requested

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,11 @@ package 'sun-jre6' do
   action :install
 end
 
+package 'sun-jdk6' do
+  action :install
+  only_if { node['modcloth_java']['install_jdk'] == true }
+end
+
 cookbook_file '/opt/local/bin/java' do
   mode '0755'
   source 'java'
